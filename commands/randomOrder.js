1bf29bf152232e477bf5012a36682c3a7041b62b
Shawn Thompson <sthompson7720@gmail.com>
2024-03-13 15:57:47 -0400

    fixed an error where duplicate interactions were received when using the /randomorder channel command through re-ordering code. It previously relied on hoisting, which made receiving the proper interaction inconsistent at-best.

--- commands/randomOrder.js
+++ commands/randomOrder.js
@@ -1,6 +1,21 @@
 import { buildEmbed } from "../functions/buildEmbed.js";
 import { shuffle } from "../functions/shuffle.js";
 
+export async function randomorder(interaction){
+    const scope = interaction.options.getString('scope');
+
+    switch (scope) {
+        case 'server':
+            await randomOrderServer(interaction);
+            break;
+        case 'channel':
+            await randomOrderChannel(interaction);
+            break;
+        default:
+            await interaction.reply({ content: 'Invalid Scope Provided.', ephemeral: true});
+    }
+}
+
 export async function randomOrderServer(interaction){
     await interaction.guild.members.fetch();
     const members = Array.from(interaction.guild.members.cache.values());
@@ -47,19 +62,4 @@ export async function randomOrderChannel(interaction){
     const embed = buildEmbed('Random Order (Channel)', response);
 
     await interaction.reply({ embeds: [embed] });
-}
-
-export async function randomorder(interaction){
-    const scope = interaction.options.getString('scope');
-
-    switch (scope) {
-        case 'server':
-            await randomOrderServer(interaction);
-            break;
-        case 'channel':
-            await randomOrderChannel(interaction);
-            break;
-        default:
-            await interaction.reply({ content: 'Invalid Scope Provided.', ephemeral: true});
-    }
 }
