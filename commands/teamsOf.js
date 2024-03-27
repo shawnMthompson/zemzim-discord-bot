1285069e31e3dd47ad95dc3411224186e595d330
Shawn Thompson <sthompson7720@gmail.com>
2024-03-27 12:54:38 -0400

    Ctrl C + Ctrl V code from randomOrder.js to teamsOf.js for fetching channel or server members and shuffling them.

--- commands/teamsOf.js
+++ commands/teamsOf.js
@@ -18,12 +18,32 @@ export async function teamsof(interaction){
 export async function teamsOfServer(interaction){
     const membersPerTeam = interaction.options.getString('#');
 
+    await interaction.guild.members.fetch();
+    const members = Array.from(interaction.guild.members.cache.values());
+
+    const nonBotMembers = members.filter(member => !member.user.bot)
+    const usernames = nonBotMembers.map(member => member.user.username);
+
+    const randomOrder = shuffle(usernames);
+
+    // Implement Teams Formatting
+
     //const embed = buildEmbed('Teams of # (Server)', response);   # Replace '#' with user-input teams of # and use `
 
     await interaction.reply({ embeds: [embed] });
 }
 export async function teamsOfChannel(interaction){
     const membersPerTeam = interaction.options.getString('#');
+
+    const channel = interaction.channel;
+
+    await channel.guild.members.fetch();
+    const members = channel.members.filter(member => !member.user.bot);
+    const usernames = members.map(member => member.user.username);
+
+    const randomOrder = shuffle(usernames)
+
+    // Implement Teams Formatting
 
     //const embed = buildEmbed('Teams of # (Channel)', response);   # Replace '#' with user-input teams of # and use `
 
