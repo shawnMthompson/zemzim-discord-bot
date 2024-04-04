e1519bb3d7550961bebca86ef69d391867ce939f
Shawn Thompson <sthompson7720@gmail.com>
2024-04-03 21:09:44 -0400

    added /poll description to displayHelp.js command

--- commands/displayHelp.js
+++ commands/displayHelp.js
@@ -3,7 +3,7 @@ import { EmbedBuilder } from "discord.js";
 export async function displayHelp(interaction){
     const embed = new EmbedBuilder()
     .setTitle('Zemzim Commands')
-    .setDescription('/coinflip [sides]\n/randomorder [scope]\n/teamsof [scope] [num]')
+    .setDescription('/coinflip [sides]\n/randomorder [scope]\n/teamsof [scope] [num]\n/poll [question] [choice1, choice2, ...]')
     .setColor('#b200ed')
     .setTimestamp();
 
