29e31cfcc3efc4438f51db6c85a3a2cc653f3866
Shawn Thompson <sthompson7720@gmail.com>
2024-08-04 14:23:31 -0400

    chore: Updated formatting via prettier

--- commands/randomOrder.js
+++ commands/randomOrder.js
@@ -1,59 +1,62 @@
 import { buildEmbed } from "../functions/buildEmbed.js";
 import { shuffle } from "../functions/shuffle.js";
 
-export async function randomorder(interaction){
-    const scope = interaction.options.getString('scope');
+export async function randomorder(interaction) {
+  const scope = interaction.options.getString("scope");
 
-    switch (scope) {
-        case 'server':
-            await randomOrderServer(interaction);
-            break;
-        case 'channel':
-            await randomOrderChannel(interaction);
-            break;
-        default:
-            await interaction.reply({ content: 'Invalid Scope Provided.', ephemeral: true});
-    }
+  switch (scope) {
+    case "server":
+      await randomOrderServer(interaction);
+      break;
+    case "channel":
+      await randomOrderChannel(interaction);
+      break;
+    default:
+      await interaction.reply({
+        content: "Invalid Scope Provided.",
+        ephemeral: true,
+      });
+  }
 }
 
-function formatUsernames(randomOrder){
-    let order = 1;
-    let response = '';
-    for (const username of randomOrder) {
-        response += `Order ${order}: ${username}\n`;
-        order++;
-    }
-    return response;
+function formatUsernames(randomOrder) {
+  let order = 1;
+  let response = "";
+  for (const username of randomOrder) {
+    response += `Order ${order}: ${username}\n`;
+    order++;
+  }
+  return response;
 }
 
-export async function randomOrderServer(interaction){
-    await interaction.guild.members.fetch();
-    const members = Array.from(interaction.guild.members.cache.values());
+export async function randomOrderServer(interaction) {
+  await interaction.guild.members.fetch();
+  const members = Array.from(interaction.guild.members.cache.values());
 
-    const nonBotMembers = members.filter(member => !member.user.bot)
-    const usernames = nonBotMembers.map(member => member.user.username);
+  const nonBotMembers = members.filter((member) => !member.user.bot);
+  const usernames = nonBotMembers.map((member) => member.user.username);
 
-    const randomOrder = shuffle(usernames);
+  const randomOrder = shuffle(usernames);
 
-    let response = formatUsernames(randomOrder);
+  let response = formatUsernames(randomOrder);
 
-    const embed = buildEmbed('Random Order (Server)', response);
+  const embed = buildEmbed("Random Order (Server)", response);
 
-    await interaction.reply({ embeds: [embed] });
+  await interaction.reply({ embeds: [embed] });
 }
 
-export async function randomOrderChannel(interaction){
-    const channel = interaction.channel;
+export async function randomOrderChannel(interaction) {
+  const channel = interaction.channel;
 
-    await channel.guild.members.fetch();
-    const members = channel.members.filter(member => !member.user.bot);
-    const usernames = members.map(member => member.user.username);
+  await channel.guild.members.fetch();
+  const members = channel.members.filter((member) => !member.user.bot);
+  const usernames = members.map((member) => member.user.username);
 
-    const randomOrder = shuffle(usernames)
+  const randomOrder = shuffle(usernames);
 
-    let response = formatUsernames(randomOrder);
+  let response = formatUsernames(randomOrder);
 
-    const embed = buildEmbed('Random Order (Channel)', response);
+  const embed = buildEmbed("Random Order (Channel)", response);
 
-    await interaction.reply({ embeds: [embed] });
+  await interaction.reply({ embeds: [embed] });
 }
--- commands/teamsOf.js
+++ commands/teamsOf.js
@@ -1,77 +1,90 @@
 import { buildEmbed } from "../functions/buildEmbed.js";
 import { shuffle } from "../functions/shuffle.js";
 
-export async function teamsof(interaction){
-    const scope = interaction.options.getString('scope');
+export async function teamsof(interaction) {
+  const scope = interaction.options.getString("scope");
 
-    switch (scope) {
-        case 'server':
-            await teamsOfServer(interaction);
-            break;
-        case 'channel':
-            await teamsOfChannel(interaction);
-            break;
-        default:
-            await interaction.reply({ content: 'Invalid Scope Provided.', ephemeral: true});
-    }
+  switch (scope) {
+    case "server":
+      await teamsOfServer(interaction);
+      break;
+    case "channel":
+      await teamsOfChannel(interaction);
+      break;
+    default:
+      await interaction.reply({
+        content: "Invalid Scope Provided.",
+        ephemeral: true,
+      });
+  }
 }
 
-export async function teamsOfServer(interaction){
-    const membersPerTeam = interaction.options.getString('num');
-    if (membersPerTeam < 1) {
-        await interaction.reply({ content: 'Please provide a number greater than 0.', ephemeral: true});
-        return;
-    }
+export async function teamsOfServer(interaction) {
+  const membersPerTeam = interaction.options.getString("num");
+  if (membersPerTeam < 1) {
+    await interaction.reply({
+      content: "Please provide a number greater than 0.",
+      ephemeral: true,
+    });
+    return;
+  }
 
-    await interaction.guild.members.fetch();
-    const members = Array.from(interaction.guild.members.cache.values());
+  await interaction.guild.members.fetch();
+  const members = Array.from(interaction.guild.members.cache.values());
 
-    const nonBotMembers = members.filter(member => !member.user.bot)
-    const usernames = nonBotMembers.map(member => member.user.username);
+  const nonBotMembers = members.filter((member) => !member.user.bot);
+  const usernames = nonBotMembers.map((member) => member.user.username);
 
-    const randomOrder = shuffle(usernames);
+  const randomOrder = shuffle(usernames);
 
-    const teams = [];
-    const teamSize = parseInt(membersPerTeam);
-    const numTeams = Math.ceil(nonBotMembers.length / teamSize);
+  const teams = [];
+  const teamSize = parseInt(membersPerTeam);
+  const numTeams = Math.ceil(nonBotMembers.length / teamSize);
 
-    for (let i = 0; i < numTeams; i++) {
-        const teamMembers = randomOrder.slice(i * teamSize, (i + 1) * teamSize);
-        teams.push(teamMembers);
-    }
+  for (let i = 0; i < numTeams; i++) {
+    const teamMembers = randomOrder.slice(i * teamSize, (i + 1) * teamSize);
+    teams.push(teamMembers);
+  }
 
-    const response = teams.map((team, index) => `Team ${index + 1}: ${team.join(", ")}`).join("\n");
-    
-    const embed = buildEmbed(`Teams of ${membersPerTeam} (Server)`, response);
+  const response = teams
+    .map((team, index) => `Team ${index + 1}: ${team.join(", ")}`)
+    .join("\n");
 
-    await interaction.reply({ embeds: [embed] });
+  const embed = buildEmbed(`Teams of ${membersPerTeam} (Server)`, response);
+
+  await interaction.reply({ embeds: [embed] });
 }
-export async function teamsOfChannel(interaction){
-    const channel = interaction.channel;
-    const membersPerTeam = interaction.options.getString('num');
-    if (membersPerTeam < 1) {
-        await interaction.reply({ content: 'Please provide a number greater than 0.', ephemeral: true});
-        return;
-    }
+export async function teamsOfChannel(interaction) {
+  const channel = interaction.channel;
+  const membersPerTeam = interaction.options.getString("num");
+  if (membersPerTeam < 1) {
+    await interaction.reply({
+      content: "Please provide a number greater than 0.",
+      ephemeral: true,
+    });
+    return;
+  }
 
-    await channel.guild.members.fetch();
-    const members = channel.members.filter(member => !member.user.bot);
-    const usernames = members.map(member => member.user.username);
+  await channel.guild.members.fetch();
+  const members = channel.members.filter((member) => !member.user.bot);
+  const usernames = members.map((member) => member.user.username);
 
-    const randomOrder = shuffle(usernames)
+  const randomOrder = shuffle(usernames);
 
-    const teams = [];
-    const teamSize = parseInt(membersPerTeam);
-    const numTeams = Math.ceil(usernames.length / teamSize);
+  const teams = [];
+  const teamSize = parseInt(membersPerTeam);
+  const numTeams = Math.ceil(usernames.length / teamSize);
 
-    for (let i = 0; i < numTeams; i++) {
-        const teamMembers = randomOrder.slice(i * teamSize, (i + 1) * teamSize);
-        teams.push(teamMembers);
-    }
+  for (let i = 0; i < numTeams; i++) {
+    const teamMembers = randomOrder.slice(i * teamSize, (i + 1) * teamSize);
+    teams.push(teamMembers);
+  }
 
-    let response = teams.map((team, index) => `Team ${index + 1}: ${team.join(", ")}`).join("\n");
+  let response = teams
+    .map((team, index) => `Team ${index + 1}: ${team.join(", ")}`)
+    .join("\n");
 
-    const embed = buildEmbed(`Teams of ${membersPerTeam} (Channel)`, response);
+  const embed = buildEmbed(`Teams of ${membersPerTeam} (Channel)`, response);
 
-    await interaction.reply({ embeds: [embed] });
+  await interaction.reply({ embeds: [embed] });
 }
